c6752f8c0b145ef85b5f36f04c074b6acf50eaf3
poodlepp <1943543964@qq.com>
2024-09-06 11:28:01 +0800

    manager mint

--- src/UniswapV3Manager.sol
+++ src/UniswapV3Manager.sol
@@ -3,8 +3,63 @@ pragma solidity ^0.8.26;
 
 import "./UniswapV3Pool.sol";
 import "./interfaces/IERC20.sol";
+import "./interfaces/IUniswapV3Manager.sol";
+import "./interfaces/IUniswapV3Pool.sol";
+import "./lib/TickMath.sol";
+import "./lib/LiquidityMath.sol";
 
-contract UniswapV3Manager {
+contract UniswapV3Manager is IUniswapV3Manager {
+    error SlippageCheckFailed(uint256 amount0, uint256 amount1);
+
+    address public immutable factory;
+
+    constructor(address factory_) {
+        factory = factory_;
+    }
+
+    function mint2(
+        MintParams calldata params
+    ) public returns (uint256 amount0, uint256 amount1) {
+        address poolAddress = PoolAddress.computeAddress(
+            factory,
+            params.token0,
+            params.token1,
+            params.tickSpacing
+        );
+        IUniswapV3Pool pool = IUniswapV3Pool(poolAddress);
+
+        (uint160 sqrtPriceX96, ) = pol.slot0();
+        uint160 sqrtPriceLowerX96 = TickMath.getSqrtRatioAtTick(
+            params.lowerTick
+        );
+        uint160 sqrtPriceUpperX96 = TickMath.getSqrtRatioAtTick(
+            params.upperTick
+        );
+        uint128 liquidity = LiquidityMath.getLiquidityForAmounts(
+            sqrtPriceX96,
+            sqrtPriceLowerX96,
+            sqrtPriceUpperX96,
+            params.amount0Desired,
+            params.amount1Desired
+        );
+        (amount0, amount1) = pool.mint(
+            msg.sender,
+            params.lowerTick,
+            params.upperTick,
+            liquidity,
+            abi.encode(
+                IUniswapV3Pool.CallbackData({
+                    token0: pool.token0(),
+                    token1: pool.token1(),
+                    payer: msg.sender
+                })
+            )
+        );
+        if (amount0 < params.amount0Min || amount1 < params.amount1Min) {
+            revert SlippageCheckFailed(amount0, amount1);
+        }
+    }
+
     function mint(
         address poolAddress_,
         int24 lowerTick,
